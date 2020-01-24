f19d225d200228be362b3190e9daff345f6126a5
alizahir@live.com <alizahir@live.com>
2020-01-24 12:52:32 +0500

    Modifies url for axios

--- blogs/urls.py
+++ blogs/urls.py
@@ -2,7 +2,7 @@ from .views import BlogsList, UserCreate, LoginView, BlogDetail, BlogCreate
 from django.urls import path, include
 from knox import views as knox_views
 urlpatterns = [
-  path('blogs', BlogsList.as_view(), name='blog_list'),
+  path('blogs/', BlogsList.as_view(), name='blog_list'),
   path('blog', BlogCreate.as_view(), name='blog_create'),
   path('blogs/<int:blog_id>', BlogDetail.as_view(), name='blog_detail' ),
   path('user', UserCreate.as_view(), name='user_create'),
